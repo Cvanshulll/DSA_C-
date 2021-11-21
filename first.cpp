89424c6eae1341c1442ddbfb620c70130a89e58d
Cvanshulll <cvanshul09@gmail.com>
2021-11-22 00:23:46 +0530

    added

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Nmaste Dunita:-)\n" ;
+
+    int a= 'a';
+    cout<<a<<endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    unsigned int a = -112;   //112
+    cout<<a<<"\n";
+}
