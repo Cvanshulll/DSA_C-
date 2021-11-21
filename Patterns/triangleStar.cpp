444a3dae24d0969342b8fdce6d30b298fc136c31
Cvanshulll <cvanshul09@gmail.com>
2021-11-22 00:32:17 +0530

    pattern

---
+++ Patterns/triangleStar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n ; 
+cin>>n;
+int i =0; 
+while(i<n)
+{
+  int j =0; 
+  while(j<=i)
+  {
+    cout<<"*"<<" ";
+    j++;
+  }
+  cout<<'\n';
+  i++;
+}
+return 0;
+}
