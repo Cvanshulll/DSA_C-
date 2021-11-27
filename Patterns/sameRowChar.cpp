c960028e4bc8d3a7c11465eb0b348a909255e4ee
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2021-11-27 12:00:40 +0530

    Create sameRowChar.cpp

---
+++ Patterns/sameRowChar.cpp
@@ -0,0 +1,31 @@
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+/*  
+    A A A 
+    B B B
+    C C C 
+*/
+    int n;
+    cin>>n;
+    int row =1;
+
+    while(row<=n)
+    {
+        int col = 1;
+        while(col <= n)
+        {
+            char ch =  'A'+row-1;
+            cout<<ch<<" ";
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+}
