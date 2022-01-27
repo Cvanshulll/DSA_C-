f74b61168d81b319449520dbd042e5a99e2abc1e
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2022-01-27 22:08:32 +0530

    Update Selection_Sort.cpp

--- Sorting/Selection_Sort.cpp
+++ Sorting/Selection_Sort.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int  selectionSort(int arr[], int n )
+void  selectionSort(int arr[], int n )
 {
     for(int i =0; i<n; i++)
     {
         int minIndex = i;
         for(int j =i+1; j<n; j++)
         {
-            if(arr[j]<minIndex)
+            if(arr[j]<arr[minIndex])
                minIndex=j;
         }
         swap(arr[minIndex],arr[i]);
     }
 }
+
 int main()
 {
     int n ;
@@ -23,5 +24,11 @@ int main()
    {
        cin>>x;
    }
+
    selectionSort(arr,n);
+
+   for(auto &x:arr)
+   {
+     cout<<x<<" ";
+   }
 }
