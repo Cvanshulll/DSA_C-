59435e603bbfb567d033c0e6131ed09cd085caa0
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2022-02-17 00:03:30 +0530

    Create MaxOccurChar.cpp

---
+++ Strings/MaxOccurChar.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+
+class Solution
+{
+    public:
+    //Function to find the maximum occurring character in a string.
+    char getMaxOccuringChar(string str)
+    {
+        // Your code here
+        int arr[26]={0};
+         int no = 0;
+        for(int i =0; i<str.size(); i++)
+        {
+            char ch = str[i];
+           
+            no = ch-'a';
+            arr[no]++;
+            
+        }
+        int maxi = INT_MIN,ans =-1;
+        for(int i =0; i<26; i++)
+        {
+            if(maxi<arr[i])
+            {
+                ans =i;
+                maxi =arr[i];
+            }
+        }
+        
+        char res = 'a'+ans;
+        return res;
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+   
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+    	Solution obj;
+        cout<< obj.getMaxOccuringChar(str)<<endl;
+    }
+}  // } Driver Code Ends
