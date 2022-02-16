a67bf0d23306b0b7d1771064a799b8941c98b659
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2022-02-17 00:37:33 +0530

    Create ReplaceSpaces.cpp

---
+++ Strings/ReplaceSpaces.cpp
@@ -0,0 +1,22 @@
+string replaceSpaces(string &str){
+	// Write your code here.
+//     string t = "";
+    for(int i =0; i<str.size(); i++)
+    {
+        if(str[i]==' ')
+        { 
+            str= str.substr(0,i)+"@40"+str.substr(i+1,str.length());
+        } 
+        // try replace() function too
+}
+//             t.push_back('@');
+//              t.push_back('4');
+//              t.push_back('0');
+//         }
+//         else 
+//         {
+// //              t.push_back(str[i]);
+//         }
+//     }
+    return str;
+}
